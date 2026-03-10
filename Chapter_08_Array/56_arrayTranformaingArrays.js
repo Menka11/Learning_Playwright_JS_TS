fad2a9f13a411f36cdb892918f3854971c992156
Menka11 <34205858+Menka11@users.noreply.github.com>
2026-03-10 10:50:26 +0530

    Tasks of 10-03-2026

--- Chapter_08_Array/56_arrayTranformaingArrays.js
+++ Chapter_08_Array/56_arrayTranformaingArrays.js
@@ -2,7 +2,7 @@ let scores = [45, 82, 91, 60, 73];
 
 //map- transforms every element, return new array
 
-let grades = scores.map(s => s > 70 ? "pass" : "fail");
+let grades = scores.map(s => s > 70);
 console.log(grades);
 
 
@@ -19,4 +19,4 @@ console.log(total);
 //flat - flatters nested arrays
 
 let nested = [[1,2],[3,4],[5]];
-console.log(nested.flat());
+console.log(nested.flat());   //converts nested array elements to single array ex. [[1,2],[3,4],[5]] => [1,2,3,4,5]
