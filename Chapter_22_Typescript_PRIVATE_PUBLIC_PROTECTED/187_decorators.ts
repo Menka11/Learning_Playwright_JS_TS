e90e279a6a4cb79c98dcc78a07b5d94fc1286cd3
menka patel <menka@gmail.com>
2026-04-18 10:04:48 +0530

    decorators

---
+++ Chapter_22_Typescript_PRIVATE_PUBLIC_PROTECTED/187_decorators.ts
@@ -0,0 +1,12 @@
+function Role (role: string){
+    return function(constructor: Function){
+        constructor.prototype.role = role;
+    };
+}
+@Role("admin")
+class Admin{
+
+}
+
+const admin = new Admin();
+console.log((admin as any).role);
